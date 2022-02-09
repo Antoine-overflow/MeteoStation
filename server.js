b2d84d15c5e81494576138d521eeb38a653bb713
antoine-overflow <antoine@DESKTOP-VKFREPH.localdomain>
2022-02-09 16:23:30 +0100

    new database working nicely

--- server.js
+++ server.js
@@ -8,25 +8,25 @@ const Influx = require('influx');
 const influx= new Influx.InfluxDB({
     host: 'localhost',
     database: 'meteodb',
-    port:8086,
-    schema: [
-        {
-            measurement: 'piensg30',
-            fields: { temperature: Influx.FieldType.FLOAT, 
-                pressure: Influx.FieldType.FLOAT, 
-                humidity: Influx.FieldType.FLOAT, 
-                luminosity: Influx.FieldType.FLOAT, 
-                wind_heading: Influx.FieldType.FLOAT, 
-                wind_speed_avg: Influx.FieldType.FLOAT,
-                wind_speed_max: Influx.FieldType.FLOAT, 
-                wind_speed_min: Influx.FieldType.FLOAT, 
-                lat: Influx.FieldType.FLOAT,
-                lon: Influx.FieldType.FLOAT,
-                alt: Influx.FieldType.FLOAT,
-            },
-            tags: [ 'host' ]
-        }
-    ]
+    // port:8086,
+    // schema: [
+    //     {
+    //         measurement: 'piensg30',
+    //         fields: { temperature: Influx.FieldType.FLOAT, 
+    //             pressure: Influx.FieldType.FLOAT, 
+    //             humidity: Influx.FieldType.FLOAT, 
+    //             luminosity: Influx.FieldType.FLOAT, 
+    //             wind_heading: Influx.FieldType.FLOAT, 
+    //             wind_speed_avg: Influx.FieldType.FLOAT,
+    //             wind_speed_max: Influx.FieldType.FLOAT, 
+    //             wind_speed_min: Influx.FieldType.FLOAT, 
+    //             lat: Influx.FieldType.FLOAT,
+    //             lon: Influx.FieldType.FLOAT,
+    //             alt: Influx.FieldType.FLOAT,
+    //         },
+    //         tags: [ 'host' ]
+    //     }
+    // ]
 });
 
 function loop(){
@@ -37,6 +37,25 @@ function loop(){
             if(err){
                 return console.log(err);
             }
+            // console.log(res);
+            // var date = '';
+            // for(var i=9; i<33; i++){
+            //     if(i==19){date+=' '}
+            //     else if(i==32){date+=''}
+            //     else{date += res[i]}
+            // }
+            // var res2 = [];
+            // for(var i=35; i<res.length;i++){
+            //     res2[i-35]=res[i];
+            // }
+            // var res3 = '';
+            // for(var i=0;i<res2.length;i++){
+            //     res3 += res2[i];
+            // }
+            // res4 = '{"date":'+'"'+date+'"'+','+res3;
+            // console.log(res4);
+            // console.log(JSON.parse(res));
+            // console.log(JSON.parse(res4));
             resolve(JSON.parse(res));
             // console.log(res);
         });
@@ -69,29 +88,54 @@ function loop(){
     // }));
 
     Promise.all(promises).then(promises => {
-        // console.log(promises);
+        console.log('.');
         setTimeout(loop, 1000);
-        // console.log(promises[1]);
         influx.writePoints([
             {
-                measurement: "piensg30",
-                time: promises[0].date,
-                fields:{
-                    temperature: promises[0].measure[0].value,
-                    pressure: promises[0].measure[1].value,
-                    humidity: promises[0].measure[2].value,
-                    luminosity:promises[0].measure[3].value, 
-                    wind_heading: promises[0].measure[4].value, 
-                    wind_speed_avg: promises[0].measure[5].value, 
-                    wind_speed_max: promises[0].measure[6].value, 
-                    wind_speed_min: promises[0].measure[7].value,
-                    lat: promises[1].lat,
-                    lon: promises[1].lon,
-                    alt: promises[1].alt
-                }
+                measurement: "temperature",
+                fields: {date: promises[0].date, value: promises[0].measure[0].value},
             }
         ]);
+        influx.writePoints([
+            {
+                measurement: "pressure",
+                fields: {date: promises[0].date, value: promises[0].measure[1].value},
+            }
+        ]);
+        influx.writePoints([
+            {
+                measurement: "humidity",
+                fields: {date: promises[0].date, value: promises[0].measure[2].value},
+            }
+        ]);
+        influx.writePoints([
+            {
+                measurement: "luminosity",
+                fields: {date: promises[0].date, value: promises[0].measure[3].value},
+            }
+        ]);
+        influx.writePoints([
+            {
+                measurement: "wind_heading",
+                fields: {date: promises[0].date, value: promises[0].measure[4].value},
+            }
+        ]);
+        influx.writePoints([
+            {
+              measurement: 'wind',
+              fields: {date: promises[0].date, wind_avg: promises[0].measure[5].value, wind_min: promises[0].measure[6].value, wind_max: promises[0].measure[7].value}
+            }
+        ]);
+        influx.writePoints([
+            {
+              measurement: 'gpsposition',
+              fields: {date: promises[0].date, lat: promises[1].lat, lon: promises[1].lon, alt: promises[1].alt}
+            }
+        ]);
+        
     });
 }
 
 loop();
+console.log("You are now adding data to the database \n");
+console.log("While you don't press CTRL + C data are added to the database every seconds");
